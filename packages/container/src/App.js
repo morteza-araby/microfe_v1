7422b0bc3d17da50b99b71227de79e21f49e73aa
Morteza Araby <morteza.araby@gmail.com>
2023-05-01 16:07:41 +0200

    Init Commit, With github actions

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+
+export default function App() {
+  return <div>
+    <h1>Hi There</h1>
+    <hr></hr>
+    <MarketingApp />
+  </div>;
+}
---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Mount function to start up the app
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+// if we are in development and in isoloation
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// we are running thru container, export the mount
+export { mount };
